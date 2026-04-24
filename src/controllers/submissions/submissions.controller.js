06d52a8e79602ad77419a9f8109666de489f78f6
azterfree <asterworl@gmail.com>
2026-04-23 19:48:23 -0600

    Implementando registrar placeSubmission en firestore y storage, solo falta mejorar la UI de AddPlacesScreen y meter los horarios xd

--- src/controllers/submissions/submissions.controller.js
+++ src/controllers/submissions/submissions.controller.js
@@ -1,20 +1,40 @@
+import { firebaseAdmin } from "../../config/firebase.js";
 import createPlaceSubmissionService from "../../services/submissions/create/createPlaceSubmission.service.js";
 
 export default async function createPlaceSubmissionController(req, res) {
   try {
-    const result = await createPlaceSubmissionService(req.body);
+    const authHeader = req.headers.authorization || "";
+
+    const idToken = authHeader.startsWith("Bearer ")
+      ? authHeader.replace("Bearer ", "")
+      : null;
+
+    if (!idToken) {
+      return res.status(401).json({
+        ok: false,
+        message: "Token no proporcionado.",
+      });
+    }
+
+    const decodedToken = await firebaseAdmin.auth().verifyIdToken(idToken);
+    const uid = decodedToken.uid;
+
+    const result = await createPlaceSubmissionService({
+      payload: req.body,
+      uid,
+    });
 
     return res.status(201).json({
       ok: true,
-      message: "Place submission recibida correctamente",
+      message: "Place submission creada correctamente.",
       data: result,
     });
   } catch (error) {
-    console.error("Error en createPlaceSubmissionController:", error);
+    console.error("createPlaceSubmissionController error:", error);
 
     return res.status(500).json({
       ok: false,
-      message: "Error al recibir place submission",
+      message: "No se pudo crear la place submission.",
     });
   }
 }
--- src/services/submissions/create/createPlaceSubmission.service.js
+++ src/services/submissions/create/createPlaceSubmission.service.js
@@ -1,10 +1,25 @@
-export default async function createPlaceSubmissionService(payload) {
+import { db, FieldValue } from "../../../config/firebase.js";
+
+export default async function createPlaceSubmissionService({ payload, uid }) {
   console.log("========== PLACE SUBMISSION RECIBIDA ==========");
   console.log(JSON.stringify(payload, null, 2));
   console.log("===============================================");
 
+  const submissionToSave = {
+    ...payload,
+    createdBy: uid,
+    source: "mobile",
+    status: "in_review",
+    createdAt: FieldValue.serverTimestamp(),
+  };
+
+  await db
+    .collection("placeSubmissions")
+    .doc(payload.placeSubmissionId)
+    .set(submissionToSave);
+
   return {
-    received: true,
-    payload,
+    id: payload.placeSubmissionId,
+    ...submissionToSave,
   };
 }
