645f27084af8abb0f31a0e1246812b40a55411ba
D4NT333 <151494657+D4NT333@users.noreply.github.com>
2026-08-01 19:59:35 -0600

    agregnado pollos yo que se

--- src/routes/jobs/jobs.routes.js
+++ src/routes/jobs/jobs.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 
 import runPlacesActivityStatusJobController from "../../controllers/places/activity/runPlacesActivityStatusJob.controller.js";
+import resetPlacesActivityCheckpointsController from "../../controllers/places/activity/resetPlacesActivityCheckpoints.controller.js";
 
 const router = Router();
 
 router.post("/places/activity-status",runPlacesActivityStatusJobController);
+
+router.post("/places/activity-checkpoints",resetPlacesActivityCheckpointsController);
 
 export default router;
